fa4839222d18d4a78bd247ffe6ac2868d1b71e31
Sneha Patil <psneha010@gmail.com>
2023-03-02 17:02:29 +0530

    added new validation

--- Linearreg.py
+++ Linearreg.py
@@ -1,1 +1,9 @@
 print("Model fits the data")
+subid = ['7377AZ3737377','8838AZ3888383']
+for i in subid:
+    print(i)
+    value = i[4:6]
+    if value == 'AZ':
+        print("test is pass")
+    else:
+        print("test fails")
